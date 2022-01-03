a374f7199f4db4fdb1fa8e743e36142c4ac0ea4a
KiralyTamas <tomaszpest@gmail.com>
2022-01-03 15:43:52 +0100

    Egymás mellé kell illeszteni a Frame-eket.

--- GUI.py
+++ GUI.py
@@ -1,13 +1,20 @@
 import PySimpleGUI as psg
-from PySimpleGUI import VerticalSeparator
 
 
 def reglog():
-    layout1 = [psg.Column([[psg.Text('User Login Name:')], [psg.Input(size=(20, 1), key='--NAME--')], [psg.Text('User Password:')], [psg.Input(size=(20, 1), key='--PASSWORD--')], [psg.Button('Registration')]], element_justification='center',pad=(0,0))]
-    vertical=VerticalSeparator()
-    layout2=[psg.Column([[psg.Text('Login Name:')], [psg.Input(size=(20, 1), key='--NAME--')], [psg.Text('Password:')], [psg.Input(size=(20, 1), key='--PASSWORD--')], [psg.Button('Log In')]], element_justification='center', pad=(0,0))]
-    layout=[layout1,vertical,layout2]
-    window = psg.Window('Reg and Log In', layout)
+    layout = [[psg.Frame('Reg', 
+            [[psg.Text('User Login Name:')],
+            [psg.Input(size=(20, 1), key='--NAME--')],
+            [psg.Text('User Password:')],
+            [psg.Input(size=(20, 1), key='--PASSWORD--')],
+            [psg.Button('Registration')]])],
+        [psg.Frame('Log',
+            [[psg.Text('Login Name:')],
+            [psg.Input(size=(20, 1), key='--NAME--')],
+            [psg.Text('Password:')],
+            [psg.Input(size=(20, 1), key='--PASSWORD--')],
+            [psg.Button('Log In')]])]]
+    window = psg.Window('Reg and Log In', layout, size=(400,400))
     while True:
         event, values = window.read()
         if event == psg.WINDOW_CLOSED or event == 'Quit':
