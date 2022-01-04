aea4a8371f18c61c7a7b43732d6167406849523d
Király Tamás <tomaszpest@gmail.com>
2022-01-04 21:53:22 +0100

    Kissebb előrelépés.

--- source_files/register.py
+++ source_files/register.py
@@ -1,18 +1,16 @@
+import json
 import pandas as pd
 import os
 
-def reg(name):
+def reg(user_name,password):
     if os.path.exists("database")==False:
         os.mkdir("database")
     if os.path.exists("database/user_datas.json")==False:
         open("database/user_datas.json", "x")
-    data = pd.read_json("database/user_datas.json")
-    nevek=data['user_name'].values
-    for index,row in enumerate(nevek):
-        if name in row:
-            print(row)
-            print(index)
-        else:
-            continue
-        
-reg("Minato")
+    status=False
+    raw_data={'password':[password],'gyermek':[status]}
+    data=pd.DataFrame(raw_data,columns=['password','status'],index=[user_name])
+    data.to_json(r'database/user_datas.json',orient='index')
+    print("Készen vagyok")
+
+reg("Sasuke","Sakura")
