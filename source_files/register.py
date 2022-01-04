9ca60db55e461ef318ea967dffd0b22e5085b840
KiralyTamas <tomaszpest@gmail.com>
2022-01-04 15:09:33 +0100

    Folyt.Köv.

--- source_files/register.py
+++ source_files/register.py
@@ -7,12 +7,12 @@ def reg(name):
     if os.path.exists("database/user_datas.json")==False:
         open("database/user_datas.json", "x")
     data = pd.read_json("database/user_datas.json")
-    nevek=data['user_name'].values+data['password'].values
+    nevek=data['user_name'].values
     for index,row in enumerate(nevek):
-        print(row)
         if name in row:
+            print(row)
             print(index)
         else:
-            print("Szabad felhasználói név")
+            continue
         
 reg("Minato")
